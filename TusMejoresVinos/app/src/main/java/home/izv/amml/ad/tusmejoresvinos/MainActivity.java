f19df5eb8f0a82f1eb8dd28804abf8b2794d163e
AlfonsoMML-dev <amartinmoreno251@ieszaidinvergeles.org>
2021-11-03 19:37:46 +0100

    Creacion de las clases Vino y Csv

--- TusMejoresVinos/app/src/main/java/home/izv/amml/ad/tusmejoresvinos/MainActivity.java
+++ TusMejoresVinos/app/src/main/java/home/izv/amml/ad/tusmejoresvinos/MainActivity.java
@@ -3,12 +3,31 @@ package home.izv.amml.ad.tusmejoresvinos;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+
+import home.izv.amml.ad.tusmejoresvinos.data.Vino;
+import home.izv.amml.ad.tusmejoresvinos.util.Csv;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = MainActivity.class.getName() + "xyzyx";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initialize();
+    }
+
+    public void initialize(){
+        Vino v = new Vino(0,"Betis", "Man que pierda", "Verde", "Benito Villamarín", 15.2, 2015);
+        String csv = Csv.getCsv(v);
+        Log.v(TAG, csv);
+        Vino v2 = Csv.getVino(csv);
+        Log.v(TAG, v2.toString());
+    }
+
+    public static String getTAG() {
+        return TAG;
     }
 }
---
+++ TusMejoresVinos/app/src/main/java/home/izv/amml/ad/tusmejoresvinos/util/Csv.java
@@ -0,0 +1,50 @@
+package home.izv.amml.ad.tusmejoresvinos.util;
+
+import android.util.Log;
+
+import home.izv.amml.ad.tusmejoresvinos.MainActivity;
+import home.izv.amml.ad.tusmejoresvinos.data.Vino;
+
+public class Csv {
+    public static Vino getVino(String str){
+        String[] atributos = str.split(";");
+        Vino v = null;
+        if(atributos.length <= 7) {
+            v = new Vino();
+            try {
+                v.setId(Long.parseLong(atributos[0].trim()));
+            } catch(NumberFormatException e){
+                Log.v(MainActivity.getTAG(), e.toString());
+                System.out.println("\n" + "Error al transformar la cadena a número");
+            }
+
+            v.setNombre(atributos[1].trim());
+            v.setBodega(atributos[2].trim());
+            v.setColor(atributos[3].trim());
+            v.setOrigen(atributos[4].trim());
+            try {
+                v.setGraduacion(Double.parseDouble(atributos[4].trim()));
+            } catch (NumberFormatException e) {
+                Log.v(MainActivity.getTAG(), e.toString());
+                System.out.println("\n" + "Error al obtener la graduación");
+            }
+            try {
+                v.setFecha(Integer.parseInt(atributos[5].trim()));
+            } catch (NumberFormatException e) {
+                Log.v(MainActivity.getTAG(), e.toString());
+                System.out.println("\n" + "Error al obtener la fecha");
+            }
+        }
+        return v;
+    }
+
+    public static String getCsv(Vino v){
+        return  v.getId() + "; " +
+                v.getNombre() + "; " +
+                v.getBodega() + "; " +
+                v.getColor() + "; "  +
+                v.getOrigen() + "; " +
+                v.getGraduacion() + "; " +
+                v.getFecha() + ";" + "\n";
+    }
+}
